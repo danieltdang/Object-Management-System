472af81f7377c4852ec26c8b2ae43ddf8a4d4c95
danieltdang <dtd21b@fsu.edu>
2023-08-13 00:32:11 -0400

    Add project files.

---
+++ Objection-Management-System/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+using namespace dnn;
+
+// Constants
+const float WIDTH = 640.0;
+const float HEIGHT = 640.0;
+const float SCORE_THRESHOLD = 0.5;
+const float NMS_THRESHOLD = 0.45;
+const float CONFIDENCE_THRESHOLD = 0.45;
+
+// Text parameters
+const float FONT_SCALE = 0.8;
+const int FONT_FACE = FONT_HERSHEY_TRIPLEX;
+const int THICKNESS = 1;
+
+// Colors
+Scalar BLACK = Scalar(0, 0, 0);
+Scalar WHITE = Scalar(255, 255, 255);
+Scalar BLUE = Scalar(255, 178, 50);
+Scalar YELLOW = Scalar(0, 255, 255);
+Scalar RED = Scalar(0, 0, 255);
+
+int main()
+{
+    // Load categories
+    vector<string> categories;
+    ifstream ifs("../coco.txt");
+    string category;
+    while (getline(ifs, category))
+        categories.push_back(category);
+
+    // Load image
+    string path = "../images/fsu_logo.png";
+    string filename = path.substr(path.find_last_of('/') + 1);
+    Mat rawImg = imread(path, IMREAD_COLOR);
+    Mat resizedImg;
+    resize(rawImg, resizedImg, Size(WIDTH, HEIGHT), INTER_LINEAR);
+
+    // Load model
+    Net net;// = readNet("../models/YOLOv5s.onnx");
+
+    // Labels image with processing time
+    double freq = getTickFrequency() / 1000;
+    double ms = 1 / freq;
+    string label = format("Processing time: %.2f ms", ms);
+    putText(resizedImg, label, Point(10, 30), FONT_FACE, FONT_SCALE, BLACK, THICKNESS + 1, LINE_AA);
+    putText(resizedImg, label, Point(10, 30), FONT_FACE, FONT_SCALE, WHITE, THICKNESS, LINE_AA);
+
+    // Display window
+    imshow(filename, resizedImg);
+    waitKey(0);
+    return 0;
+}
